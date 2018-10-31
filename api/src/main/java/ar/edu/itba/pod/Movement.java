f21809f68804e9d6e7b0910dbb09df55ae19f0af
Segundo Farina <segundofarina@me.com>
2018-10-31 16:22:05 -0300

    Implemented data serializable

--- api/src/main/java/ar/edu/itba/pod/Movement.java
+++ api/src/main/java/ar/edu/itba/pod/Movement.java
@@ -1,15 +1,23 @@
 package ar.edu.itba.pod;
 
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Optional;
 
-public class Movement implements Serializable {
+public class Movement implements DataSerializable {
     //private final Optional<FlightType> flightType;
-    private final FlightType flightType;
-    private final MovementType movementType;
-    private final String sourceOASI;
-    private final String destinationOASI;
+    private FlightType flightType;
+    private MovementType movementType;
+    private String sourceOASI;
+    private String destinationOASI;
 
+    public Movement(){
+
+    }
     //public Movement(Optional<FlightType> flightType, MovementType movementType, String sourceOASI, String destinationOASI) {
     public Movement(FlightType flightType, MovementType movementType, String sourceOASI, String destinationOASI) {
         this.flightType = flightType;
@@ -33,5 +41,21 @@ public class Movement implements Serializable {
 
     public String getDestinationOASI() {
         return destinationOASI;
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+        out.writeObject(flightType);
+        out.writeObject(movementType);
+        out.writeUTF(sourceOASI);
+        out.writeUTF(destinationOASI);
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+        flightType = in.readObject();
+        movementType = in.readObject();
+        sourceOASI = in.readUTF();
+        destinationOASI = in.readUTF();
     }
 }
